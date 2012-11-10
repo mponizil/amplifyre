745b9ddf829d29479c17ce337eba374f93f68c69
mponizil <misha.ponizil@gmail.com>
2012-11-10 00:14:29 -0500

    HTTP Basic Auth everywhere except prelaunch page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   layout 'landing'
+
+  before_filter :authenticate
+
+  protected
+
+  def authenticate
+    return true if params[:controller] == 'statics' and params[:action] == 'prelaunch'
+
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'mponizil' && password == 'dinosaur56'
+    end
+  end
 end
