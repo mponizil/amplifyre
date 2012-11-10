0bd4def0256568b8da0d68f1477ca23ba6d465ef
mponizil <misha.ponizil@gmail.com>
2012-11-10 01:17:52 -0500

    Specify js and css to precompile.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,8 @@ Amplifyre::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += ['dashboard/index.js', 'landing/index.js', 'prelaunch/index.js', 'dashboard/index.css',
+    'landing/index.css', 'themes/press-play/index.css', 'themes/press-play/editor/index.css', 'prelaunch/index.css']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
