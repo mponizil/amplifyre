9b674996558aced3ae99f0d78f73e36a42a56130
mponizil <misha.ponizil@gmail.com>
2012-12-23 20:10:55 -0800

    Redirect to login page when user isn't authorized.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   layout 'landing'
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to new_user_session_url(:subdomain => false), :alert => exception.message
+  end
 end
