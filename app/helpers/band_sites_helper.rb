3e67d1c89ef0841a50dfbc28c73e45d97ee9ddf1
mponizil <misha.ponizil@gmail.com>
2013-04-01 09:34:08 -0400

    Support tld's interchangeably with subdomain.amplifyre.com. TODO: make it cleaner.

--- app/helpers/band_sites_helper.rb
+++ app/helpers/band_sites_helper.rb
@@ -1,11 +1,19 @@
 module BandSitesHelper
 
   def view_band_site_path(band_site)
-    request.protocol + band_site.slug + '.' + request.domain
+    if request.domain.starts_with?('amplifyre')
+      request.protocol + band_site.slug + '.' + request.domain
+    else
+      request.protocol + request.domain
+    end
   end
 
   def edit_band_site_path(band_site)
-    request.protocol + band_site.slug + '.' + request.domain + '/edit'
+    if request.domain.starts_with?('amplifyre')
+      request.protocol + band_site.slug + '.' + request.domain + '/edit'
+    else
+      request.protocol + request.domain
+    end
   end
 
 end
